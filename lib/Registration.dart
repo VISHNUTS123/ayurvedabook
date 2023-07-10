67caf9838d7c6d391d1dfe4cde35e0efc512c8cf
VISHNUTS123 <vishnu_b220595ee@nitc.ac.in>
2023-07-10 11:59:04 +0530

    completed the UI for user and doctor added datepicker and timepicker

--- lib/Registration.dart
+++ lib/Registration.dart
@@ -11,11 +11,24 @@ class Registration extends StatefulWidget {
 }
 
 class _RegistrationState extends State<Registration> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController genderController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  TextEditingController placeController = TextEditingController();
+  TextEditingController postController = TextEditingController();
+  TextEditingController pincodeController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+
+  ///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
       backgroundColor: Colors.transparent,
       body: Container(
         decoration: BoxDecoration(
@@ -31,105 +44,310 @@ class _RegistrationState extends State<Registration> {
           ),
         ),
         child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(15),
-                  child: Text(
-                    'REGISTER HERE !!',
-                    style: TextStyle(
-                         fontFamily: 'Caprasimo',
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.5),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Create a New User ',
-                  style: TextStyle(
-                    fontSize: 20,
-                    letterSpacing: 1.5,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 20,),
-                CircleAvatar(backgroundImage:AssetImage('images/user.jpg'),radius: 80,),
-                SizedBox(
-                  height: 50,
-                ),
-                Align(alignment: Alignment.centerLeft, child: Text('Email', style: TextStyle(fontFamily: 'Caprasimo', fontWeight: FontWeight.bold),)),
-                SizedBox(
-                  height: 5,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              width: 3, color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Align(alignment: Alignment.centerLeft, child: Text('Enter Password' ,style: TextStyle(fontFamily: 'Caprasimo', fontWeight: FontWeight.bold),)),
-                SizedBox(
-                  height: 5,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                     hintText: 'Enter a strong password',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              width: 3, color: Colors.black))),
-                  obscureText: true,
-                ),
-                SizedBox(height: 10,),
-                Align(alignment: Alignment.centerLeft, child: Text('Confirm Password' ,style: TextStyle(fontFamily: 'Caprasimo', fontWeight: FontWeight.bold),)),
-                SizedBox(
-                  height: 5,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Confirm your Password',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              width: 3, color: Colors.black))),
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 30, // <-- SEE HERE
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    width: 300,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border.all(),
-                      color: Colors.black,
+          padding: const EdgeInsets.all(10),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      'REGISTER HERE !!',
+                      style: TextStyle(
+                          fontFamily: 'Caprasimo',
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.5),
                     ),
-                    child: Center(child: Text('Register',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,fontFamily: ' Caprasimo',color: Colors.white),)),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-
-
-
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Create a New User ',
+                    style: TextStyle(
+                      fontSize: 20,
+                      letterSpacing: 1.5,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: AssetImage('images/user.jpg'),
+                    radius: 80,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'NAME',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'AGE',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller:ageController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'GENDER',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: genderController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Email',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'CONTACT NUMBER',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: phoneController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'ADDRESS',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: addressController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'PLACE',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: placeController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'POST OFFICE',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: postController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'PINCODE',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: pincodeController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'USERNAME',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Enter Password',
+                        style: TextStyle(
+                            fontFamily: 'Caprasimo',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        hintText: 'Enter a strong password',
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.black))),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 30, // <-- SEE HERE
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: 300,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(),
+                        color: Colors.black,
+                      ),
+                      child: Center(
+                          child: Text(
+                        'Register',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            fontFamily: ' Caprasimo',
+                            color: Colors.white),
+                      )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-    ),
       ),
     );
   }
