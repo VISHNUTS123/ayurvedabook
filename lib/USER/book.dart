9a7bee9d5200c50c9240bb5e8b9f9329b46a60b6
VISHNUTS123 <vishnu_b220595ee@nitc.ac.in>
2023-07-17 12:50:24 +0530

    completed yser registration , login , updation of details of the user using ngrok , anaconda and upi

--- lib/USER/book.dart
+++ lib/USER/book.dart
@@ -11,66 +11,88 @@ class Booking extends StatefulWidget {
 
 class _BookingState extends State<Booking> {
   List<DoctorDetails> doctorDetailsList = [];
-  void bookmenu() {
+
+
+  void bookmenu(DoctorDetails selectedDoctor) {
     DateTime selectedDate = DateTime.now();
     TimeOfDay selectedTime = TimeOfDay.now();
 
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return SingleChildScrollView(
-          child: Container(
-            height: 200,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Select Date and Time',
-                    style: TextStyle(fontSize: 20),
+        return IntrinsicHeight(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Select Date and Time',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.redAccent,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Available Days: ${selectedDoctor.doctoravailabledays}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Available Time: ${selectedDoctor.availabltime}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Perform the booking logic using selectedDate and selectedTime
+                            // Close the bottom sheet
+                          },
+                          child: Text('Book'),
+                        ),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: selectedDate,
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime(2100),
+                            );
+                            if (pickedDate != null &&
+                                pickedDate != selectedDate) {
+                              setState(() {
+                                selectedDate = pickedDate;
+                              });
+                            }
+                          },
+                          child: Text('Select Date'),
+                        ),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final TimeOfDay? pickedTime = await showTimePicker(
+                              context: context,
+                              initialTime: selectedTime,
+                            );
+                            if (pickedTime != null &&
+                                pickedTime != selectedTime) {
+                              setState(() {
+                                selectedTime = pickedTime;
+                              });
+                            }
+                          },
+                          child: Text('Select Time'),
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Perform the booking logic using selectedDate and selectedTime
-                      // Close the bottom sheet
-                    },
-                    child: Text('Book'),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () async {
-                      final DateTime? pickedDate = await showDatePicker(
-                        context: context,
-                        initialDate: selectedDate,
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(2100),
-                      );
-                      if (pickedDate != null && pickedDate != selectedDate) {
-                        setState(() {
-                          selectedDate = pickedDate;
-                        });
-                      }
-                    },
-                    child: Text('Select Date'),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () async {
-                      final TimeOfDay? pickedTime = await showTimePicker(
-                        context: context,
-                        initialTime: selectedTime,
-                      );
-                      if (pickedTime != null && pickedTime != selectedTime) {
-                        setState(() {
-                          selectedTime = pickedTime;
-                        });
-                      }
-                    },
-                    child: Text('Select Time'),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         );
@@ -102,9 +124,9 @@ class _BookingState extends State<Booking> {
         backgroundColor: const Color(0xff764abc),
       ),
       drawer: UserDrawer(),
-      body: Center(
+      body: Align(
+        alignment: Alignment.topCenter,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Padding(
               padding: EdgeInsets.all(10),
@@ -117,42 +139,43 @@ class _BookingState extends State<Booking> {
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: doctorDetailsList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  DoctorDetails doctor = doctorDetailsList[index];
-                  return Container(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: ListTile(
-                      leading: const Icon(Icons.health_and_safety_outlined),
-                      trailing: TextButton(
-                        onPressed: () {
-                          bookmenu();
-                        },
-                        child: const Text(
-                          "BOOK",
-                          style: TextStyle(color: Colors.green, fontSize: 15),
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: ListView.builder(
+                  itemCount: doctorDetailsList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    DoctorDetails doctor = doctorDetailsList[index];
+                    return Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: ListTile(
+                        leading: const Icon(Icons.health_and_safety_outlined),
+                        trailing: TextButton(
+                          onPressed: () {
+                            bookmenu(doctor);
+                          },
+                          child: const Text(
+                            "BOOK",
+                            style: TextStyle(color: Colors.green, fontSize: 15),
+                          ),
+                        ),
+                        title: Text(
+                          'Dr.${doctor.doctorname}',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(doctor.doctorspecialization),
+                            Text(
+                                'Experience: ${doctor.doctorexperience} years'),
+                            Text('Availability: ${doctor.doctoravailabledays}'),
+                            // Add more subtitles here
+                          ],
                         ),
                       ),
-                      title: Text(
-                       'Dr.${ doctor.doctorname}',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ), //used to print the doctorname
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(doctor.doctorspecialization),
-                          Text('Experience: ${doctor.doctorexperience} years'),
-                          Text('Availability: ${doctor.doctoravailabledays}'),
-                          Text('Available Time: ${doctor.availabltime}'),
-                          // Add more subtitles here
-                        ],
-                      ),
-                      // subtitle: Text(doctor.doctorspecialization),//used to print doctor speccialization
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             )
           ],
@@ -160,6 +183,4 @@ class _BookingState extends State<Booking> {
       ),
     );
   }
-
-  // Mock data for demonstration purposes
 }
